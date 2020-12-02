66fbabe570b03564db2e48b4cbe0ceaf92004c19
hengyunabc <hengyunabc@gmail.com>
2020-12-02 11:10:37 +0800

    clean code

--- bytekit-core/src/main/java/com/alibaba/bytekit/utils/IOUtils.java
+++ bytekit-core/src/main/java/com/alibaba/bytekit/utils/IOUtils.java
@@ -1,18 +1,12 @@
 package com.alibaba.bytekit.utils;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
-import java.util.Enumeration;
-import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 /**
@@ -92,62 +86,5 @@ public class IOUtils {
             return ioe;
         }
         return null;
-    }
-
-    public static void unzip(String zipFile, String extractFolder) throws IOException {
-        File file = new File(zipFile);
-        ZipFile zip = null;
-        try {
-            int BUFFER = 1024 * 8;
-
-            zip = new ZipFile(file);
-            String newPath = extractFolder;
-
-            new File(newPath).mkdir();
-            Enumeration<? extends ZipEntry> zipFileEntries = zip.entries();
-
-            // Process each entry
-            while (zipFileEntries.hasMoreElements()) {
-                // grab a zip file entry
-                ZipEntry entry = (ZipEntry) zipFileEntries.nextElement();
-                String currentEntry = entry.getName();
-
-                File destFile = new File(newPath, currentEntry);
-                // destFile = new File(newPath, destFile.getName());
-                File destinationParent = destFile.getParentFile();
-
-                // create the parent directory structure if needed
-                destinationParent.mkdirs();
-
-                if (!entry.isDirectory()) {
-                    BufferedInputStream is = null;
-                    BufferedOutputStream dest = null;
-                    try {
-                        is = new BufferedInputStream(zip.getInputStream(entry));
-                        int currentByte;
-                        // establish buffer for writing file
-                        byte data[] = new byte[BUFFER];
-
-                        // write the current file to disk
-                        FileOutputStream fos = new FileOutputStream(destFile);
-                        dest = new BufferedOutputStream(fos, BUFFER);
-
-                        // read and write until last byte is encountered
-                        while ((currentByte = is.read(data, 0, BUFFER)) != -1) {
-                            dest.write(data, 0, currentByte);
-                        }
-                        dest.flush();
-                    } finally {
-                        close(dest);
-                        close(is);
-                    }
-
-                }
-
-            }
-        } finally {
-            close(zip);
-        }
-
     }
 }
