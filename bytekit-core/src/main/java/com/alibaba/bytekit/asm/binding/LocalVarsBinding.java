4a085917632d846ac7bf23db22b054d2cfe63652
isadliliying@163.com <isadliliying@163.com>
2023-01-28 22:17:07 +0800

    add exclude-pattern to LocalVarsBinding and LocalVarNamesBinding

--- bytekit-core/src/main/java/com/alibaba/bytekit/asm/binding/LocalVarsBinding.java
+++ bytekit-core/src/main/java/com/alibaba/bytekit/asm/binding/LocalVarsBinding.java
@@ -1,7 +1,10 @@
 package com.alibaba.bytekit.asm.binding;
 
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
+import com.alibaba.bytekit.utils.MatchUtils;
 import com.alibaba.deps.org.objectweb.asm.Type;
 import com.alibaba.deps.org.objectweb.asm.tree.AbstractInsnNode;
 import com.alibaba.deps.org.objectweb.asm.tree.InsnList;
@@ -15,13 +18,30 @@ import com.alibaba.bytekit.utils.AsmOpUtils;
  */
 public class LocalVarsBinding extends Binding{
 
+    private String excludePattern;
+
+    public LocalVarsBinding(String excludePattern) {
+        this.excludePattern = excludePattern;
+    }
+
+    public LocalVarsBinding() {
+    }
+
     @Override
     public void pushOntoStack(InsnList instructions, BindingContext bindingContext) {
 
         AbstractInsnNode currentInsnNode = bindingContext.getLocation().getInsnNode();
 
-        List<LocalVariableNode> results = AsmOpUtils
-                .validVariables(bindingContext.getMethodProcessor().getMethodNode().localVariables, currentInsnNode);
+        List<LocalVariableNode> localVariables = new LinkedList<LocalVariableNode>(bindingContext.getMethodProcessor().getMethodNode().localVariables);
+        if (excludePattern != null && !excludePattern.isEmpty()){
+            Iterator<LocalVariableNode> it = localVariables.iterator();
+            while(it.hasNext()){
+                LocalVariableNode localVariableNode = it.next();
+                if (MatchUtils.wildcardMatch(localVariableNode.name,excludePattern)) it.remove();
+            }
+        }
+
+        List<LocalVariableNode> results = AsmOpUtils.validVariables(localVariables, currentInsnNode);
 
         AsmOpUtils.push(instructions, results.size());
         AsmOpUtils.newArray(instructions, AsmOpUtils.OBJECT_TYPE);
@@ -45,4 +65,11 @@ public class LocalVarsBinding extends Binding{
         return AsmOpUtils.OBJECT_ARRAY_TYPE;
     }
 
+    public String getExcludePattern() {
+        return excludePattern;
+    }
+
+    public void setExcludePattern(String excludePattern) {
+        this.excludePattern = excludePattern;
+    }
 }
