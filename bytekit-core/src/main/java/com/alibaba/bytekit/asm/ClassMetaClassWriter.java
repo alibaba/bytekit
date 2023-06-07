2d0c450f64bd95130d65c56c9edaa1127311069b
hengyunabc <hengyunabc@gmail.com>
2023-06-07 11:47:02 +0800

    ClassMetaClassWriter support special classLoader. #37

--- bytekit-core/src/main/java/com/alibaba/bytekit/asm/ClassMetaClassWriter.java
+++ bytekit-core/src/main/java/com/alibaba/bytekit/asm/ClassMetaClassWriter.java
@@ -29,8 +29,13 @@ public class ClassMetaClassWriter extends ClassWriter {
      * 缺省情况下，ClassWriter将会在getCommonSuperClass方法中计算这些，通过在加载这两个类进入虚拟机时，使用反射API来计算。
      * 但是，如果你将要生成的几个类相互之间引用，这将会带来问题，因为引用的类可能还不存在。
      * 在这种情况下，你可以重写getCommonSuperClass方法来解决这个问题。
-     *
-     * 采用尝试用 ClassLoader#getResource 的方式来读取 .class 文件，再计算 CommonSuperClass
+     * 
+     * <pre>
+     * 1. 采用尝试用 ClassLoader#getResource 的方式来读取 .class 文件，再计算 CommonSuperClass
+     * 2. 因为计算过程中，可能需要一直向上查找 super 类的 .class 文件，中间可能失败
+     * 3. 失败后则回退到原来的 ClassWriter#getCommonSuperClass(String, String) ，但ClassLoader是指定的。本类重写了 getClassLoader() 函数
+     * </pre>
+     * 
      */
     @Override
     protected String getCommonSuperClass(String type1, String type2) {
@@ -56,8 +61,21 @@ public class ClassMetaClassWriter extends ClassWriter {
         } else {
             do {
                 classMeta1 = classMeta1.getSuperclass();
+                if (classMeta1 == null) {
+                    // TODO 是直接返回 "java/lang/Object"，还是尝试用指定 classLoader 查找
+                    return getCommonSuperClass(type1, type2);
+                }
             } while (!classMeta1.isAssignableFrom(classMeta2));
             return classMeta1.getInternalClassName();
         }
     }
+
+    /**
+     * 重写获取 classLoader 函数，原来的逻辑是直接用 ClassMetaClassWriter 的 classLoader
+     */
+    @Override
+    protected ClassLoader getClassLoader() {
+        return classLoader;
+    }
+
 }
