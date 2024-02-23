6168849da9e6ccf1d118ecda6df0dc373ea2d25a
hengyunabc <hengyunabc@gmail.com>
2024-02-23 16:01:06 +0800

    improve InterceptorParserUtils

--- bytekit-core/src/main/java/com/alibaba/bytekit/asm/interceptor/annotation/InterceptorParserUtils.java
+++ bytekit-core/src/main/java/com/alibaba/bytekit/asm/interceptor/annotation/InterceptorParserUtils.java
@@ -1,43 +1,49 @@
 package com.alibaba.bytekit.asm.interceptor.annotation;
 
-import com.alibaba.deps.org.objectweb.asm.Type;
+import java.lang.reflect.Method;
+import java.util.List;
+
 import com.alibaba.bytekit.asm.binding.Binding;
 import com.alibaba.bytekit.asm.interceptor.InterceptorMethodConfig;
 import com.alibaba.bytekit.asm.interceptor.InterceptorProcessor;
 import com.alibaba.bytekit.asm.location.LocationMatcher;
-
-import java.lang.reflect.Method;
-import java.util.List;
+import com.alibaba.deps.org.objectweb.asm.Type;
 
 public class InterceptorParserUtils {
 
-    public static InterceptorProcessor createInterceptorProcessor(
-            Method method,
-            LocationMatcher locationMatcher,
-            boolean inline,
-            Class<? extends Throwable> suppress,
-            Class<?> suppressHandler) {
+    public static InterceptorProcessor createInterceptorProcessor(Method method, LocationMatcher locationMatcher) {
+        return createInterceptorProcessor(method, locationMatcher, true);
+    }
 
-        InterceptorProcessor interceptorProcessor = new InterceptorProcessor(method.getDeclaringClass().getClassLoader());
+    public static InterceptorProcessor createInterceptorProcessor(Method method, LocationMatcher locationMatcher,
+            boolean inline) {
+        return createInterceptorProcessor(method, locationMatcher, inline, null, null);
+    }
 
-        //locationMatcher
+    public static InterceptorProcessor createInterceptorProcessor(Method method, LocationMatcher locationMatcher,
+            boolean inline, Class<? extends Throwable> suppress, Class<?> suppressHandler) {
+
+        InterceptorProcessor interceptorProcessor = new InterceptorProcessor(
+                method.getDeclaringClass().getClassLoader());
+
+        // locationMatcher
         interceptorProcessor.setLocationMatcher(locationMatcher);
 
-        //interceptorMethodConfig
+        // interceptorMethodConfig
         InterceptorMethodConfig interceptorMethodConfig = new InterceptorMethodConfig();
         interceptorProcessor.setInterceptorMethodConfig(interceptorMethodConfig);
         interceptorMethodConfig.setOwner(Type.getInternalName(method.getDeclaringClass()));
         interceptorMethodConfig.setMethodName(method.getName());
         interceptorMethodConfig.setMethodDesc(Type.getMethodDescriptor(method));
 
-        //inline
+        // inline
         interceptorMethodConfig.setInline(inline);
 
-        //bindings
+        // bindings
         List<Binding> bindings = BindingParserUtils.parseBindings(method);
         interceptorMethodConfig.setBindings(bindings);
 
-        //errorHandlerMethodConfig
+        // errorHandlerMethodConfig
         if (suppress != null) {
             InterceptorMethodConfig errorHandlerMethodConfig = ExceptionHandlerUtils.errorHandlerMethodConfig(suppress,
                     suppressHandler);
